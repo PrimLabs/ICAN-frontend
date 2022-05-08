6f6aa45558ac8fd43943f3d0ac1f0ba1d12f6a0e
beyond 009 <1249413181@qq.com>
2022-05-08 13:19:41 +0800

    add home

--- src/views/Siderbar/index.tsx
+++ src/views/Siderbar/index.tsx
@@ -11,116 +11,121 @@ import "tailwindcss/tailwind.css";
 import { RootState } from "@/redux/store";
 
 export const SiderBar = () => {
-    const [profile, setProfile] = React.useState<boolean>(false);
-    const [hubs, setHubs] = React.useState<any>();
-    const { buckets } = useSelector((state: RootState) => state);
-    const history = useHistory();
-    const {
-        IILogIn,
-        plugLogIn,
-        isAuth,
-        principal,
-        logOut,
-        subAccountId,
-    }: {
-        IILogIn: Function;
-        plugLogIn: Function;
-        isAuth: boolean;
-        principal: Principal;
-        logOut: Function;
-        subAccountId: string;
-    } = useAuth();
-    useEffect(() => {
-        principal && setHubs(Storage.getBucketsStorage(String(principal)));
-        principal && console.log(Storage.getBucketsStorage(String(principal)));
-    }, [principal, buckets]);
-    return (
-        <>
-            <ModalWrap open={profile} setOpen={setProfile}>
-                <Profile open={profile} />
-            </ModalWrap>
-            <div className="fixed h-full w-[120px]  top-1 rounded shadow-2xl bg-white bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-25 border border-gray-200 flex flex-col justify-start items-center ">
-                <Gap height={40} />
-                {isAuth ? (
-                    <>
-                        <div
-                            data-tooltip-target="tooltip-profile"
-                            data-tooltip-placement="right"
-                            className="bg-blue-100 mb-[40px] rounded h-[90px] w-[90px] flex justify-center items-center hover:shadow hover: cursor-pointer"
-                            onClick={() => {
-                                setProfile(true);
-                            }}
-                        >
-                            <Icon name="account" />
-                        </div>
-                        <div
-                            id="tooltip-profile"
-                            role="tooltip"
-                            className="inline-block absolute invisible z-10 py-2 px-3 text-2xl font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-                        >
-                            user info
-                            <div
-                                className="tooltip-arrow"
-                                data-popper-arrow
-                            ></div>
-                        </div>
-                    </>
-                ) : (
-                    <div
-                        className="bg-blue-100 mb-[40px] rounded h-[90px] w-[90px] flex justify-center items-center hover:shadow hover: cursor-pointer"
-                        onClick={() => IILogIn()}
-                    >
-                        <Icon name="login" />
-                    </div>
-                )}
-                <div
-                    data-tooltip-target="tooltip-right"
-                    data-tooltip-placement="right"
-                    className="bg-blue-100 mb-[40px] rounded h-[90px] w-[90px] flex justify-center items-center hover:shadow hover: cursor-pointer"
-                    onClick={() => {
-                        history.push("/createhub");
-                    }}
-                >
-                    <Icon name="add" />
-                </div>
-                <div
-                    id="tooltip-right"
-                    role="tooltip"
-                    className="inline-block absolute invisible z-10 py-2 px-3 text-2xl font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-                >
-                    create hub
-                    <div className="tooltip-arrow" data-popper-arrow></div>
-                </div>
-                {isAuth &&
-                    buckets.map((v, k) => {
-                        return (
-                            <>
-                                <div
-                                    data-tooltip-target={"tooltip-hub" + v[0]}
-                                    data-tooltip-placement="right"
-                                    key={k}
-                                    className="bg-blue-100 mb-[40px] rounded h-[90px] w-[90px] flex justify-center items-center hover:shadow hover: cursor-pointer"
-                                    onClick={() => {
-                                        history.push(`/hub/${v[0]}/${v[1]}`);
-                                    }}
-                                >
-                                    <Icon name="station" />
-                                </div>
-                                <div
-                                    id={"tooltip-hub" + v[0]}
-                                    role="tooltip"
-                                    className="inline-block absolute invisible z-10 py-2 px-3 text-2xl font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-                                >
-                                    hub {v[0]}
-                                    <div
-                                        className="tooltip-arrow"
-                                        data-popper-arrow
-                                    ></div>
-                                </div>
-                            </>
-                        );
-                    })}
+  const [profile, setProfile] = React.useState<boolean>(false);
+  const [hubs, setHubs] = React.useState<any>();
+  const { buckets } = useSelector((state: RootState) => state);
+  const history = useHistory();
+  const {
+    IILogIn,
+    plugLogIn,
+    isAuth,
+    principal,
+    logOut,
+    subAccountId,
+  }: {
+    IILogIn: Function;
+    plugLogIn: Function;
+    isAuth: boolean;
+    principal: Principal;
+    logOut: Function;
+    subAccountId: string;
+  } = useAuth();
+  useEffect(() => {
+    principal && setHubs(Storage.getBucketsStorage(String(principal)));
+    principal && console.log(Storage.getBucketsStorage(String(principal)));
+  }, [principal, buckets]);
+  return (
+    <>
+      <ModalWrap open={profile} setOpen={setProfile}>
+        <Profile open={profile} />
+      </ModalWrap>
+
+      <div className="fixed h-full w-[120px]  top-1 rounded shadow-2xl bg-white bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-25 border border-gray-200 flex flex-col justify-start items-center ">
+        <Gap height={40} />
+        <div
+          data-tooltip-target="tooltip-profile"
+          data-tooltip-placement="right"
+          className="bg-blue-100 mb-[40px] rounded h-[90px] w-[90px] flex justify-center items-center hover:shadow hover: cursor-pointer"
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          <Icon name="home" />
+        </div>
+        {isAuth ? (
+          <>
+            <div
+              data-tooltip-target="tooltip-profile"
+              data-tooltip-placement="right"
+              className="bg-blue-100 mb-[40px] rounded h-[90px] w-[90px] flex justify-center items-center hover:shadow hover: cursor-pointer"
+              onClick={() => {
+                setProfile(true);
+              }}
+            >
+              <Icon name="account" />
             </div>
-        </>
-    );
+            <div
+              id="tooltip-profile"
+              role="tooltip"
+              className="inline-block absolute invisible z-10 py-2 px-3 text-2xl font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+            >
+              user info
+              <div className="tooltip-arrow" data-popper-arrow></div>
+            </div>
+          </>
+        ) : (
+          <div
+            className="bg-blue-100 mb-[40px] rounded h-[90px] w-[90px] flex justify-center items-center hover:shadow hover: cursor-pointer"
+            onClick={() => IILogIn()}
+          >
+            <Icon name="login" />
+          </div>
+        )}
+        <div
+          data-tooltip-target="tooltip-right"
+          data-tooltip-placement="right"
+          className="bg-blue-100 mb-[40px] rounded h-[90px] w-[90px] flex justify-center items-center hover:shadow hover: cursor-pointer"
+          onClick={() => {
+            history.push("/createhub");
+          }}
+        >
+          <Icon name="add" />
+        </div>
+        <div
+          id="tooltip-right"
+          role="tooltip"
+          className="inline-block absolute invisible z-10 py-2 px-3 text-2xl font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+        >
+          create hub
+          <div className="tooltip-arrow" data-popper-arrow></div>
+        </div>
+        {isAuth &&
+          buckets.map((v, k) => {
+            return (
+              <>
+                <div
+                  data-tooltip-target={"tooltip-hub" + v[0]}
+                  data-tooltip-placement="right"
+                  key={k}
+                  className="bg-blue-100 mb-[40px] rounded h-[90px] w-[90px] flex justify-center items-center hover:shadow hover: cursor-pointer"
+                  onClick={() => {
+                    history.push(`/hub/${v[0]}/${v[1]}`);
+                  }}
+                >
+                  <Icon name="station" />
+                </div>
+                <div
+                  id={"tooltip-hub" + v[0]}
+                  role="tooltip"
+                  className="inline-block absolute invisible z-10 py-2 px-3 text-2xl font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+                >
+                  hub {v[0]}
+                  <div className="tooltip-arrow" data-popper-arrow></div>
+                </div>
+              </>
+            );
+          })}
+      </div>
+    </>
+  );
 };
